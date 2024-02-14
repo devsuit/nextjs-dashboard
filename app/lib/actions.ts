ca5957e664b27b7525f0b3cb04f7463cf15bdd2e
Syed Qalb-e-Abbas <sqabbas1994@gmail.com>
2024-02-14 11:36:43 +0500

    Errors handling...
    handle errors gracefully

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,11 +29,18 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
+try {
+  
   await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
+} catch (error) {
+  return {
+    message: 'Database Error: Failed to Create Invoice.',
+  };
+}
+
 revalidatePath('/dashboard/invoices');
 redirect('/dashboard/invoices');
 
@@ -52,19 +59,29 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
+ try {
+  
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+} catch (error) {
+  return { message: 'Database Error: Failed to Update Invoice.' };
+}
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 //------------------------------------------------------------Delete Action-------
 export async function deleteInvoice(id: string) {
+  // throw new Error('Failed to Delete Invoice');
+  try {
+
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+  return { message: 'Deleted Invoice.' };
+} catch (error) {
+  return { message: 'Database Error: Failed to Delete Invoice.' };
+}
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
@@ -145,6 +146,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  // noStore();
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -161,6 +163,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+    console.log(invoice); // Invoice is an empty array []
 
     return invoice[0];
   } catch (error) {
